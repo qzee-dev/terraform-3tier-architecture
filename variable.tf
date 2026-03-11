6f6e65d32c63e8ab7e1c557c6e2040e0353b2a68
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-03-11 08:27:38 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,36 @@
+variable "cidr_block" {
+  default = "this is the value for block ip"
+}
+
+variable "subnet1" {
+    default = "this is subnet1 value"
+  
+}
+
+variable "subnet2" {
+    default = "value  for subnet2"
+  
+}
+variable "subnet3" {
+    default = "value of subnet3"
+  
+}
+
+variable "subnet4" {
+    default = "value for subnet4"
+  
+}
+
+variable "ami1"{
+
+default = "value for ec2 ami"
+
+}
+variable "instance_type1" {
+    default = "value for instance_type"
+}
+  
+
+
+
+  
