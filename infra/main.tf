4c0a204871bbb912a2ca0b619360f13637ad6530
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-03-31 10:01:54 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -74,7 +74,7 @@ resource "aws_route_table_association" "public_subnet_1a_cidr" {
 
 }
 resource "aws_route_table_association" "public_subnet_1b_cidr" {
-  subnet_id      = aws_subnet.public_subnet_1a_cidr.id
+  subnet_id      = aws_subnet.public_subnet_1b_cidr.id
   route_table_id = aws_route_table.public.id
 
 }
