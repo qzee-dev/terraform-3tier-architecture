ec598007de763f69bd9fe365302b63c4381194df
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-04-09 16:46:32 +0100

    Update web-asg.tf

--- infra/web-asg.tf
+++ infra/web-asg.tf
@@ -1,19 +1,23 @@
-
 resource "aws_autoscaling_group" "web_asg" {
-  name                      = "web-asg"
+  name               = "web-asg"
+  min_size           = 2
+  max_size           = 4
+  desired_capacity   = 2
+  vpc_zone_identifier = [
+    aws_subnet.subnet1.id,
+    aws_subnet.subnet2.id
+  ]
+
   launch_template {
     id      = aws_launch_template.web_lt.id
     version = "$Latest"
   }
-  min_size                  = 2
-  max_size                  = 4
-  desired_capacity          = 2
-  vpc_zone_identifier       = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
+
   health_check_type         = "ELB"
   health_check_grace_period = 30
   force_delete              = true
-
-  target_group_arns = [aws_lb_target_group.app_tg.arn]
 
   tag {
     key                 = "Name"
