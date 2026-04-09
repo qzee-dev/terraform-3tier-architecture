9e709964aefb166ac29aeaf22f0c535a9d3db229
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-04-09 18:39:48 +0100

    Update cloudwatch_alarm.tf

--- infra/cloudwatch_alarm.tf
+++ infra/cloudwatch_alarm.tf
@@ -18,6 +18,25 @@ resource "aws_cloudwatch_metric_alarm" "memory_high" {
   alarm_actions = [aws_autoscaling_policy.memory_scale_out.arn]
 }
 ###########################################################################
+  Cloudwatch Container Alarm
+###########################################################################
+resource "aws_cloudwatch_metric_alarm" "container_restarts" {
+  alarm_name          = "container-restart-detected"
+  alarm_description   = "Triggers when any Docker container restarts on ASG instances"
+  
+  namespace           = "CWAgent"
+  metric_name         = "container_restarts"
+  statistic           = "Sum"
+  period              = 300
+  evaluation_periods  = 1
+  threshold           = 1
+  comparison_operator = "GreaterThanThreshold"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web_asg.name
+  }
+}
+###########################################################################
 Cloud watch Alb Latency Alarm
 ###########################################################################
 resource "aws_cloudwatch_metric_alarm" "alb_latency_high" {
@@ -55,6 +74,27 @@ resource "aws_cloudwatch_metric_alarm" "network_out_high" {
 ###########################################################################
 Container restart Alarm
 ###########################################################################
+resource "aws_cloudwatch_metric_alarm" "container_restarts_detected" {
+  alarm_name          = "${var.project_name}-${var.environment}-container-restarts"
+  alarm_description   = "Container restart detected on ASG instances"
+  namespace           = "CWAgent"
+  metric_name         = "container_restarts"
+  statistic           = "Sum"
+  period              = 300
+  evaluation_periods  = 1
+  threshold           = 1
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web_asg.name
+  }
+
+  alarm_actions = [
+    aws_sns_topic.ops_alerts.arn
+  ]
+}
+
 
 
 
