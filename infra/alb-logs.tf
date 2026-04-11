942d0137d8e0797e07063d968ea654341644e74d
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-04-11 20:01:36 +0100

    Update alb-logs.tf

--- infra/alb-logs.tf
+++ infra/alb-logs.tf
@@ -27,3 +27,73 @@ resource "aws_s3_bucket_policy" "alb_logs_policy" {
     ]
   })
 }
+#################################################
+🛡️ 1. S3 Bucket Policy for ALB
+###################################################
+resource "aws_s3_bucket_policy" "bucket1_alb_logs_policy" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowALBLogDelivery"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        }
+
+        Action = "s3:PutObject"
+
+        Resource = "${aws_s3_bucket.bucket1.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      }
+    ]
+  })
+}
+
+data "aws_caller_identity" "current" {}
+
+#############################################
+🔐 2. KMS Key Policy for ALB
+Since i am using a custom key from AWS Key Management Service, ALB must be allowed to use it.
+###############################################
+
+resource "aws_kms_key" "s3_kms_key" {
+  description             = "KMS key for S3 bucket encryption"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowRootAccount"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowALBUseOfKey"
+        Effect = "Allow"
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:GenerateDataKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
